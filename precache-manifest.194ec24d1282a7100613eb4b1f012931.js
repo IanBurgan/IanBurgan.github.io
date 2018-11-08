1af9771b9116fb28a51eabb387a3fac68bf7bfab
Ian Burgan <burganian@gmail.com>
2018-11-08 17:07:07 -0500

    initial commit, a react build

---
+++ precache-manifest.194ec24d1282a7100613eb4b1f012931.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ca1ed66312cb6a87adf0",
+    "url": "/static/js/main.ca1ed663.chunk.js"
+  },
+  {
+    "revision": "1b5cec309744c32b0912",
+    "url": "/static/js/1.1b5cec30.chunk.js"
+  },
+  {
+    "revision": "ca1ed66312cb6a87adf0",
+    "url": "/static/css/main.5078e2db.chunk.css"
+  },
+  {
+    "revision": "289f438645ec2fbcd4d16e441e1255f0",
+    "url": "/index.html"
+  }
+];
