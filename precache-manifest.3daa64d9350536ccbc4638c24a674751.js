b7b452b8ce3d5b197ebf9073e05085ac3b0c05a3
Ian Burgan <burganian@gmail.com>
2018-11-28 02:32:11 -0500

    updated with slides link

---
+++ precache-manifest.3daa64d9350536ccbc4638c24a674751.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "d2a33b2aca0ecd0ddd20",
+    "url": "/static/js/main.d2a33b2a.chunk.js"
+  },
+  {
+    "revision": "1b5cec309744c32b0912",
+    "url": "/static/js/1.1b5cec30.chunk.js"
+  },
+  {
+    "revision": "d2a33b2aca0ecd0ddd20",
+    "url": "/static/css/main.562fda1b.chunk.css"
+  },
+  {
+    "revision": "151aa40a643a52441e9aabda2dc5fa1a",
+    "url": "/index.html"
+  }
+];
